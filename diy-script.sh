ce5291a442716c24fbe366dc889092a4480609f6
badcrazy <badcrazy@gmail.com>
2025-07-08 09:57:31 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # 添加额外插件
 git clone --depth=1 https://github.com/xiaoxiao29/luci-app-adguardhome package/luci-app-adguardhome
-
+git clone --depth=1 https://github.com/EasyTier/luci-app-easytier package/luci-app-easytier
 # 科学上网插件
 # git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
