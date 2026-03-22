fc72cd72ab9c0e11f38d451044b9c9ed6b4ae4ed
badcrazy <badcrazy@gmail.com>
2026-03-22 22:33:10 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 # 添加额外插件
-rm -rf luci/applications/luci-app-adguardhome
-rm -rf packages/net/adguardhome
 rm -rf feeds/luci/applications/luci-app-adguardhome
 rm -rf feeds/packages/net/adguardhome
 git clone --depth=1 https://github.com/sirpdboy/luci-app-adguardhome package/luci-app-adguardhome
@@ -11,7 +9,7 @@ git clone --depth=1 https://github.com/EasyTier/luci-app-easytier package/luci-a
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 # git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
-rm -rf package/helloworld
+# rm -rf package/helloworld
 # 移除 openwrt feeds 自带的核心库
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 # 移除 openwrt feeds 过时的luci版本
