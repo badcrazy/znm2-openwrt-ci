fc9166d3a985e2b0c2437e787640e94d87ab9e55
badcrazy <badcrazy@gmail.com>
2026-04-06 14:36:04 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-# 添加额外插件
+# 清理插件
+rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box} 2>/dev/null || true
+rm -rf feeds/luci/applications/luci-app-passwall 2>/dev/null || true
+rm -rf feeds/packages/net/v2ray-geodata 2>/dev/null || true
+rm -rf feeds/packages/net/{chinadns-ng,dns2socks,dns2tcp,ipt2socks,libevent2} 2>/dev/null || true
 rm -rf feeds/luci/applications/luci-app-adguardhome
 rm -rf feeds/packages/net/adguardhome
-rm -rf luci/applications/luci-app-adguardhome
-rm -rf packages/net/adguardhome
+./scripts/feeds clean
+# 添加额外插件
 git clone --depth=1 https://github.com/sirpdboy/luci-app-adguardhome package/luci-app-adguardhome
 git clone --depth=1 https://github.com/EasyTier/luci-app-easytier package/luci-app-easytier
 # 科学上网插件
- rm -rf feeds/packages/net/{xray-core, v2ray-core, v2ray-geodata, sing-box} 2>/dev/null
- rm -rf feeds/luci/applications/luci-app-passwall 2>/dev/nul
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 # git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall2 package/luci-app-passwall2
