3b66e9fa75824f7a7c4a51a7929bf3ac1e1d0ebb
badcrazy <badcrazy@gmail.com>
2026-04-06 14:37:26 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 清理插件
-rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box} 2>/dev/null || true
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls} 2>/dev/null || true
 rm -rf feeds/luci/applications/luci-app-passwall 2>/dev/null || true
 rm -rf feeds/packages/net/v2ray-geodata 2>/dev/null || true
 rm -rf feeds/packages/net/{chinadns-ng,dns2socks,dns2tcp,ipt2socks,libevent2} 2>/dev/null || true
@@ -18,12 +18,9 @@ git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall2 packag
 # git clone --depth=1 https://github.com/frazy/luci-app-singbox-configs package/luci-app-singbox
 # rm -rf package/helloworld
 # 移除 openwrt feeds 自带的核心库
-# rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
-# rm -rf packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 # 移除 openwrt feeds 过时的luci版本
 # rm -rf feeds/luci/applications/luci-app-passwall
 # rm -rf luci/applications/luci-app-passwall
 
-./scripts/feeds clean
 ./scripts/feeds update -a
 ./scripts/feeds install -a
