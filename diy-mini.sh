4b33d8aad32298ddbbaacfb8ff95c5f27275ae23
badcrazy <badcrazy@gmail.com>
2026-04-14 00:38:50 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -1,37 +1,4 @@
 #!/bin/bash
-# 清理插件
-rm -rf feeds/luci/applications/luci-app-adguardhome
-rm -rf feeds/packages/net/adguardhome
-rm -rf feeds/packages/net/chinadns-ng
-rm -rf feeds/packages/net/dns2socks
-rm -rf feeds/packages/net/dns2socks-rust
-rm -rf feeds/packages/net/dns2tcp
-rm -rf feeds/packages/net/dnsproxy
-rm -rf feeds/packages/net/gn
-rm -rf feeds/packages/net/hysteria
-rm -rf feeds/packages/net/ipt2socks
-rm -rf feeds/packages/net/microsocks
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/packages/net/naiveproxy
-rm -rf feeds/packages/net/redsocks2
-rm -rf feeds/packages/net/shadow-tls
-rm -rf feeds/packages/net/shadowsocks-libev
-rm -rf feeds/packages/net/shadowsocks-rust
-rm -rf feeds/packages/net/shadowsocksr-libev
-rm -rf feeds/packages/net/simple-obfs
-rm -rf feeds/packages/net/tcping
-rm -rf feeds/packages/net/trojan
-rm -rf feeds/packages/net/tuic-client
-rm -rf feeds/packages/net/v2ray-core
-rm -rf feeds/packages/net/v2ray-plugin
-rm -rf feeds/packages/net/xray-core
-rm -rf feeds/packages/net/xray-plugin
-rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/luci/applications/luci-app-ssr-plus
-rm -rf feeds/luci/applications/luci-app-vssr
-rm -rf feeds/luci/applications/luci-app-trojan
-rm -rf feeds/luci/applications/luci-app-v2raya
-
 # 添加 helloworld feed
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> feeds.conf.default
 
